386bc30d16fc9b3f3460bdbbc378adf5490c2a71
niujingliang <iamblessed@126.com>
2016-12-17 23:37:51 +0800

    增加loader插件

--- public/js/lib.js
+++ public/js/lib.js
@@ -76,7 +76,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + chunkId + ".app.js";
+/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"app"}[chunkId]||chunkId) + ".js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};
